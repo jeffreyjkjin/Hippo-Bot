e4a2f0486b7f5f1b20c1b75fdf9adbafb7afe3ab
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-09-16 01:30:13 -0700

    Added code to handle command interactions
    -Used conditional to check if command is valid
    -Added try/catch block to execute command

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import { CacheType, Interaction } from 'discord.js';
 import { config } from 'dotenv'
 
+import Command from './structures/Command';
 import ExtendedClient from './structures/ExtendedClient'
 
 config();
@@ -8,6 +10,29 @@ const client: ExtendedClient = new ExtendedClient();
 
 client.on('ready', () => {
     console.log(`${client.user && client.user.tag} is online!`);
+});
+
+client.on('interactionCreate', async (i: Interaction<CacheType>) => {
+    if (i.isChatInputCommand()) {
+        const command: Command = client.commands.get(i.commandName);
+
+        if (!command) {
+            await i.followUp({ 
+                content: `No command matching ${i.commandName} was found.`, 
+                ephemeral: true
+            });
+        }
+
+        try {
+            await command.execute(i);
+        } catch (e) {
+            console.error(e);
+            await i.followUp({ 
+                content: 'There was an error while executing this command.',
+                ephemeral: true
+            });
+        }
+    }
 });
 
 client.login(process.env.TOKEN);
