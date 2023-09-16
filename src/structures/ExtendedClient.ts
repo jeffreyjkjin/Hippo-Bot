dd485b4a386fd332305f623c4e6b83b28b88c1d9
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-09-16 01:28:10 -0700

    Added loadCommands method to ExtendedClient
    -Implemented method to load all command files from command folder and save them to the collection

--- src/structures/ExtendedClient.ts
+++ src/structures/ExtendedClient.ts
@@ -1,4 +1,6 @@
 import { Client, Collection, GatewayIntentBits } from 'discord.js'
+import fs from 'node:fs'
+import path from 'node:path'
 
 import Command from './Command';
 
@@ -15,5 +17,22 @@ export default class ExtendedClient extends Client {
         });
 
         this.commands = new Collection<string, Command>();
+
+        this.loadCommands();
+    }
+
+    private loadCommands() {
+        const commandPath: string = path.join(__dirname, '../commands');
+        const commandFiles: string[] = fs.readdirSync(commandPath).filter((file: string): boolean => { 
+            return file.endsWith('.ts' || '.js'); 
+        });
+
+        commandFiles.forEach((file: string) => {
+            const filePath: string = path.join(commandPath, file);
+            const command: Command = require(filePath);
+
+            console.log(`Loading command: ${command.name}`);
+            this.commands.set(command.name, command);
+        });
     }
 }
