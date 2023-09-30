73110e60fe48026da031507ba1a87cb056582ba9
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-09-29 18:08:01 -0700

    Created Button class
    -Implemented constructor and toJSON method

---
+++ src/structures/Button.ts
@@ -0,0 +1,19 @@
+import { APIButtonComponent } from "discord.js"
+import { ButtonBuilder } from "@discordjs/builders"
+
+export default class Button {
+    private data: ButtonBuilder;
+    
+    id: string;
+    execute: Function;
+
+    constructor(data: ButtonBuilder, execute: Function) {
+        this.data = data;
+        this.id = data.toJSON()['custom_id'];
+        this.execute = execute;
+    }
+
+    toJSON(): APIButtonComponent {
+        return this.data.toJSON();
+    }
+}
