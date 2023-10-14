73620f49e1803c320949cf8b8c1daac08bc5902d
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-10-14 02:11:56 -0700

    Created EventData interface
    -Created interface with attributes for an event

---
+++ src/interfaces/EventData.ts
@@ -0,0 +1,12 @@
+export default interface EventData {
+    title: string;
+    description: string;
+    time: string;
+    date: string;
+    datetime: string;
+    attendees: string[];
+    maybe: string[];
+    pass: string[];
+    image: string;
+    creator: string;
+}
