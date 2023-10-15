eb5c86a60f115ae879f442019f59ecbceebfe6c9
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-10-14 22:32:29 -0700

    Created CreateEvent modal
    -Implemented modal and function to grab data from modal after submitting

---
+++ src/modals/createevent.ts
@@ -0,0 +1,26 @@
+import { ModalSubmitInteraction } from 'discord.js'
+
+import EventEmbed from '../embeds/eventembed'
+import EventModal from '../utils/createeventmodal'
+import EventData from '../interfaces/EventData'
+import Modal from '../structures/Modal'
+
+module.exports = new Modal(
+    EventModal(),
+    async (i: ModalSubmitInteraction) => {
+        const event: EventData = {
+            title: i.fields.getTextInputValue('title'),
+            description: i.fields.getTextInputValue('description'),
+            time: i.fields.getTextInputValue('time'),
+            date: i.fields.getTextInputValue('date'),
+            datetime: null,
+            attendees: [] as string[],
+            maybe: [] as string[],
+            pass: [] as string[],
+            image: i.fields.getTextInputValue('image'),
+            creator: i.user.id
+        }
+
+        await i.reply({ embeds: [EventEmbed(i, event)] });
+    }
+);
