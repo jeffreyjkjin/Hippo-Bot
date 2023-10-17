353facb712aee9108d7038213da76854510dd16b
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-10-17 00:58:31 -0700

    Created insertEvent utility function
    -Implemented function to insert an event into mongodb
    -Throws error if fails

---
+++ src/utils/insertevent.ts
@@ -0,0 +1,12 @@
+import EventData from "../interfaces/EventData"
+
+const insertEvent = async (i: any, event: EventData) => {
+    try {
+        await i.client.mongo.db(i.guildId).collection('Events').insertOne(event);
+    }
+    catch (e) {
+        throw Error('This event could not be created.');
+    }
+}
+
+export default insertEvent;
