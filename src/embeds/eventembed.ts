fbf546ba76e0b479aaed1c76cd2cee8247d83a1f
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-10-20 00:39:06 -0700

    Updated eventEmbed to use new EventData attributes
    -Added relative time to event to time field
    -Changed attendee, maybe, and pass fields to display user tags

--- src/embeds/eventembed.ts
+++ src/embeds/eventembed.ts
@@ -1,43 +1,43 @@
-import { BaseInteraction, EmbedBuilder } from 'discord.js'
+import { BaseInteraction, EmbedBuilder, userMention } from 'discord.js'
 
 import EventData from '../interfaces/EventData'
 
-const EventEmbed = (i: BaseInteraction, event: EventData): EmbedBuilder => {
+const eventEmbed = (i: BaseInteraction, event: EventData): EmbedBuilder => {
     return new EmbedBuilder()
         .setTitle(`:calendar_spiral: ${event.title}`)
         .setDescription(event.description ? event.description : null)
         .addFields(
             { 
                 name: 'Time', 
-                value: `<t:${Date.parse(event.datetime)/1000}>`
+                value: `<t:${Date.parse(event.datetime)/1000}> (<t:${Date.parse(event.datetime)/1000}:R>)`
             },
             {
                 name: `:white_check_mark: Attendees (${event.attendees.length})`,
                 value: event.attendees.length ? event.attendees.map(
-                    (name): string => { 
-                        return '> ' + name 
+                    (id): string => { 
+                        return `> ${userMention(id)}`; 
                     }).toString().replaceAll(',', '\n') : '> -',
                 inline: true
             },
             {
                 name: `:person_shrugging: Maybe (${event.maybe.length})`,
                 value: event.maybe.length ? event.maybe.map(
-                    (name): string => {
-                        return '> ' + name 
+                    (id): string => {
+                        return `> ${userMention(id)}`;
                     }).toString().replaceAll(',', '\n') : '> -',
                 inline: true
             },
             {
                 name: `:x: Pass (${event.pass.length})`,
                 value: event.pass.length ? event.pass.map(
-                    (name): string => { 
-                        return  '> ' + name 
+                    (id): string => { 
+                        return `> ${userMention(id)}`;
                     }).toString().replaceAll(',', '\n') : '> -',
                 inline: true
             }
         )
         .setImage(event.image ? event.image : null)
-        .setFooter({ text: `Created by ${i.client.users.cache.get(event.creator).globalName}` });
+        .setFooter({ text: `Created by ${i.client.users.cache.get(event.creatorId).globalName}` });
 }
 
-export default EventEmbed;
+export default eventEmbed;
