0f580e7f5299b1ca372333f625fccd367a13994b
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-10-20 00:42:17 -0700

    Created checkevents event
    -Implemented logic to check for an event starting by querying db every minute using current time
    -For each event found, it creates an announcement message tagging all attendees and sets started attribute in db to true

---
+++ src/events/checkevents.ts
@@ -0,0 +1,54 @@
+import dayjs from 'dayjs'
+import { Channel, userMention } from 'discord.js'
+
+import startEventEmbed from '../embeds/starteventembed'
+import EventData from '../interfaces/EventData'
+import Event from '../structures/Event'
+import ExtendedClient from '../structures/ExtendedClient'
+import updateEvent from '../utils/updateevent'
+
+module.exports = new Event(
+    'ready',
+    false,
+    async (client: ExtendedClient) => {
+        setInterval(async () => {
+            client.guilds.cache.each(async (guild) => {
+                const events: EventData[] = await client.mongo.db(guild.id).collection<EventData>('Events').find({
+                    datetime: dayjs().second(0).millisecond(0).toISOString()
+                }).toArray();
+
+                events.forEach(async (event: EventData) => {
+                    if (event.started) {
+                        return;
+                    }
+
+                    let channel: Channel;
+                    try {
+                        channel = await guild.channels.fetch(event.channelId);
+                    }
+                    catch (e: any) {
+                        channel = guild.systemChannel;
+                    }
+
+                    if (channel.isTextBased()) {
+                        await channel.send({ 
+                            content: event.attendees.map((id: string) => {
+                                return userMention(id);
+                            }).toString(),
+                            embeds: [startEventEmbed(event)] 
+                        });
+                    }
+
+                    try {
+                        event.started = true;
+                        await updateEvent(client, guild.id, event);
+                    }
+                    catch (e: any) {
+                        console.log(e);
+                    }
+                });
+
+            });
+        }, 60000);
+    }
+);
