c490a9e75b711f5511119043f135679475f0512e
jeffreyjkjin <jinjeffrey24@gmail.com>
2023-12-22 01:25:29 -0800

    Updated pollEmbed
    -Fixed singleVote footer line to show up when it is disabled
    -Added logic to generate buttons from options and create new action rows when one is full
    -Fixed percentage calculation to 2 decimal points

--- src/embeds/pollembed.ts
+++ src/embeds/pollembed.ts
@@ -1,7 +1,9 @@
 import { BaseInteraction, Colors, EmbedBuilder, MessageCreateOptions } from 'discord.js'
+import { ActionRowBuilder, ButtonBuilder } from '@discordjs/builders'
 
 import PollData from '../interfaces/PollData'
 import getPollSymbol from '../utils/getpollsymbol'
+import voteButton from '../utils/votebutton'
 
 /*
      DESC: Displays poll options and buttons to allow user to vote.
@@ -13,14 +15,16 @@ import getPollSymbol from '../utils/getpollsymbol'
 const pollEmbed = (i: BaseInteraction, poll: PollData): MessageCreateOptions => {
     const creator: string = i.client.users.cache.get(poll.creatorId).globalName;
 
+    const singleVote = `${!poll.singleVote ? 'You may vote for multiple options in this poll.' : ''}`;
+    const addOptions = `${poll.addOptions && poll.options.size < 15 ? '➕ Add Option | ' : ''}`;
+
     const embed: EmbedBuilder = new EmbedBuilder()
         .setTitle(`📊 **${poll.title}**`)
         .setColor(Colors.Green)
         .setDescription(poll.description ? poll.description : null)
         .setImage(poll.image ? poll.image : null)
         .setFooter({ 
-            text: `${poll.singleVote ? 'You may vote for multiple options in this poll.' : ''}
-            ${poll.addOptions ? '➕ Add Option | ' : ''}⚙️ Settings | Created by ${creator}` 
+            text: `${singleVote}\n${addOptions}⚙️ Settings | Created by ${creator}` 
         });
 
     // sort poll options by most votes if poll is over
@@ -31,39 +35,61 @@ const pollEmbed = (i: BaseInteraction, poll: PollData): MessageCreateOptions => 
             })
         );        
     }
-            
-    let count: number = 0;
-    let symbol: string = !poll.ended ? getPollSymbol(0) : ':first_place:';
-            
+    
+    // initalize rows of buttons (grid will be up to 5x4)
+    const buttonRows: ActionRowBuilder<ButtonBuilder>[] = [new ActionRowBuilder<ButtonBuilder>()];
+    let rowNum: number = 0;
+    let buttonNum: number = 0;
+
     // generate poll options
+    let optionNum: number = 1;
+    let symbol: string;
+
     poll.options.forEach((voters: string[], option: string) => {
+        if (!poll.ended) {
+            // if poll isn't over, set symbol and add button
+            symbol = getPollSymbol(optionNum); 
+            
+            // once row is full, move to next one
+            if (buttonNum > 4) {
+                buttonNum = 0;
+                buttonRows.push(new ActionRowBuilder<ButtonBuilder>())
+                rowNum++;
+            }
+            
+            buttonRows[rowNum].addComponents(new ButtonBuilder(voteButton(option, optionNum).toJSON()));
+            buttonNum++;
+        }
+        else {
+            // set symbol for top 3 most voted options
+            if (optionNum === 0) {
+                symbol = ':first_place:';
+            }
+            if (optionNum === 1) { 
+                symbol = ':second_place:';
+            }
+            else if (optionNum === 2) {
+                symbol = ':third_place:';
+            }
+        }
+        
         // calculate percentage bar for each option
         const voteSquares: number = voters.length ? Math.round(10 * (voters.length/poll.totalVotes)) : 0;
         const voteBar: string = ':white_large_square:'.repeat(voteSquares);
         const emptyBar: string = ':black_large_square:'.repeat(10 - voteSquares);
-
+        const percentage: number = voters.length ? Math.round(voters.length/poll.totalVotes * 100)/100 : 0;
+        
         embed.addFields({
             name: `${symbol} ${option}`,
-            value: `${voteBar + emptyBar} | ${voters.length ? voters.length/poll.totalVotes : 0} (${voters.length})`
+            value: `${voteBar + emptyBar} | ${percentage}% (${voters.length})`
         });
-
-        // update symbol to next letter
-        count++;
-        if (!poll.ended) {
-            symbol = getPollSymbol(count); 
-        }
-        else {
-            if (count === 1) { 
-                symbol = ':second_place:'
-            }
-            else if (count === 2) {
-                symbol = ':third_place:'
-            }
-        }
+        
+        optionNum++;
     });
-    
+
     return {
-        embeds: [embed]
+        embeds: [embed],
+        components: buttonRows
     } as MessageCreateOptions;
 }
 
