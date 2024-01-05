8d54bcb02355370b9138e5aeab24ddf580e2bb02
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-01-04 19:14:41 -0800

    Updated startup event to initialize bot
    -Sets bots username, profile picture, and sets presence

--- src/events/startup.ts
+++ src/events/startup.ts
@@ -5,11 +5,20 @@ module.exports = new Event(
     'ready',
     true,
     /*
-         DESC: Prints a confirmation that bot is online to console.
+         DESC: Initialize bot's details.
           PRE: Bot is working correctly.
         PARAM: client - The main client of the bot.
+         POST: Prints confirmation message in console.
     */
     (client: ExtendedClient) => {
+        // initialize bot details
+        client.user.setUsername('Hippo Bot');
+        client.user.setAvatar('https://i.imgur.com/CY9g73e.png');
+        client.user.setPresence({
+            activities: [{ name: 'God | /help' }],
+            status: 'online'
+        });
+
         console.log(`${client.user && client.user.tag} is online!`);
     }
 );
